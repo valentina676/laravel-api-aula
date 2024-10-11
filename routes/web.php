0fd9b11da2932fa99a2776285ab41082a3bd974d
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-10-11 11:19:50 -0300

    aula 11/10

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,18 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
         'message' => 'OK'
     ]);
+});
+
+Route::get('/somar', function(Request $request){
+   $soma = array_sum($request->all());
+   return response()->json([
+    'message' => 'Uhuull somado com sucesso', //opcional
+    'sum' => $soma,
+   ]);
 });

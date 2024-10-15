087a94883d149d0efc2e6a51d75cee68e9a0b990
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-10-15 09:12:13 -0300

    aula15/10

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,12 @@ Route::get('/', function () {
 });
 
 Route::get('/somar', function(Request $request){
+    if (count($request->all())<1){
+        return response()->json([
+            'menssage' => 'Não há valor para somar'
+        ], 406);
+    }
+    
    $soma = array_sum($request->all());
    return response()->json([
     'message' => 'Uhuull somado com sucesso', //opcional

68213fe2c0fd0a86546671a6c1ff84e7f88ced28
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-10-29 08:49:37 -0300

    aula 29/10

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->validateCsrfTokens(except:[
+            '*'
+            //exemplo:
+            // 'api/*'
+            // 'pagina-desprotegida/action/*'
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //

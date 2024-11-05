871d03625e42400e31170099fd292ce04a10b9b7
valentina676 <153191667+valentina676@users.noreply.github.com>
2024-11-05 08:59:13 -0300

    aula05/11

---
+++ app/Http/Controllers/JWTAuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class JWTAuthController extends Controller
+{
+    public function register(Request $request) {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $data['name'],
+            'email'=> $data ['email'],
+            'password'=> Hash::make($data['password'])
+        ]);
+
+        $token = JWTAuth::fromUser($user);
+
+        return response()->json(compact('user','token'), 201);
+    }
+
+    public function login(Request $request){
+        $cred = $request->only('email', 'password');
+
+        try{
+             //se o login nao funcionou
+            if (!JWTAuth::attempt($cred)){
+                return response()->json([
+                    'error' => 'Invalid credentials'
+                ], 401);
+            }
+
+            // se chegou ate aqui, deu certo
+            $user = auth()->user();
+
+            //No caso de atribuir um papel
+            //$token = JWTAuth::claims(['role' => $user->role])->fromUser($user);
+            $token =JWTAuth::fromUser($user);
+
+            return response()->json(compact('token'));
+        } catch (JWTException $e){
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\JWTAuthController;
 use App\Http\Controllers\PeopleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -32,4 +33,8 @@ Route::prefix('/people')->group(function(){
     Route::post('/store', [PeopleController::class, 'store']);
 
     Route::post ('/storeInterest', [PeopleController::class, 'storeInterest']);
+});
+
+Route::prefix('/user')->group(function(){
+    Route::post('/register', [JWTAuthController::class, 'register']);
 });
